9a4d83ca1d4f10665752257606f4705fccf2ee99
IssacMathew1298 <117979120+IssacMathew1298@users.noreply.github.com>
2023-02-02 14:25:55 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("submit-btn").onclick = function () {
-
-    const name = document.getElementById("fname").value + " " + document.getElementById("lname").value
-
-    location.href = "/mypage/"+name;
-};
